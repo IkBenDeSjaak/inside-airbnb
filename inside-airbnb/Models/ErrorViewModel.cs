c6281126f18c2bfd8d2ec9609b9f3acb286174e7
Sjaak <sjaakrest@gmail.com>
2022-04-29 16:36:46 +0200

    add scaffolded inside-airbnb project

---
+++ inside-airbnb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inside_airbnb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
