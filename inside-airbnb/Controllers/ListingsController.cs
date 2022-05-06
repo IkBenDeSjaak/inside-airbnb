f96ccbfdc01f5b0ace48c7cb663e3f8b514cbc72
Sjaak <sjaakrest@gmail.com>
2022-05-06 11:42:47 +0200

    add scaffolded listingscontroller and add listings to layout

---
+++ inside-airbnb/Controllers/ListingsController.cs
@@ -0,0 +1,156 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using inside_airbnb.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace inside_airbnb.Controllers
+{
+    [Authorize]
+    public class ListingsController : Controller
+    {
+        private readonly InsideAirbnbContext _context;
+
+        public ListingsController(InsideAirbnbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Listings
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Listings.ToListAsync());
+        }
+
+        // GET: Listings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // GET: Listings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Listings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,HostId,HostName,NeighbourhoodGroup,Neighbourhood,Latitude,Longitude,RoomType,Price,MinimumNights,NumberOfReviews,LastReview,ReviewsPerMonth,CalculatedHostListingsCount,Availability365,NumberOfReviewsLtm,License")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(listing);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings.FindAsync(id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            return View(listing);
+        }
+
+        // POST: Listings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,HostId,HostName,NeighbourhoodGroup,Neighbourhood,Latitude,Longitude,RoomType,Price,MinimumNights,NumberOfReviews,LastReview,ReviewsPerMonth,CalculatedHostListingsCount,Availability365,NumberOfReviewsLtm,License")] Listing listing)
+        {
+            if (id != listing.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(listing);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListingExists(listing.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // POST: Listings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var listing = await _context.Listings.FindAsync(id);
+            _context.Listings.Remove(listing);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listings.Any(e => e.Id == id);
+        }
+    }
+}
